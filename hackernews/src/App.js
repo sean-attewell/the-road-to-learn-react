93a3038047a7187377c3d0d8088ad70ba3bdcea3
sean-attewell <sean.attewell@gmail.com>
2019-10-25 10:39:33 +0100

    Event handlers

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -62,12 +62,15 @@ class App extends Component {
             <span>
               {/* As you can see, the onDismiss() method in the onClick handler is enclosed by an arrow function. You can use it to peek at the objectID property of the item object and identify the item to be dismissed. An alternative way would be to define the function outside of the onClick handler and only pass the defined function to it. 
               The this object is your class instance*/}
+              {/* Not putting the method inside an arrow function wouldn't work, because the class method would be executed immediately when you open the application in the browser: */}
               <button
                 onClick={() => this.onDismiss(item.objectID)}
                 type="button"
               >
                 Dismiss
               </button>
+              {/* When using onClick={doSomething()}, the doSomething() function executes immediately when the application is opened in a browser. The expression in the handler is evaluated. Since the returned value of the function isn’t a function anymore, nothing would happen when you click the button. But using onClick={doSomething} where doSomething is a function, it would only be executed if the button is clicked. */}
+              {/* However, using onClick={this.onDismiss} wouldn’t suffice, because the item.objectID property needs to be passed to the class method to identify the item that should be dismissed. We wrap it into another function to sneak in the property. This concept is called higher-order functions */}
               <button
                 onClick={this.onClickMe}
                 type="button"
@@ -87,3 +90,18 @@ export default App;
 // What you experienced is the unidirectional data flow of React. An action is triggered in the view layer with onClick(), a function or class method modifies the local component state, and then the render() method of the component runs again to update the view.
 
 // You can remove the block body, the curly braces, with the ES6 arrow function. In a concise body, an implicit return is attached; thus, you can remove the return statement. This will happen often in this book, so be sure to understand the difference between a block body and a concise body when using arrow functions.
+
+// A function has to be passed to the element’s handler. As an example, 'onClick={console.log(item.objectID)}' - This method will run when you open the application in the browser, but not when you click the button'
+
+// The following code would only run when you click the button, a function that is executed when you trigger the handler:
+
+// <button
+//   onClick={() => console.log(item.objectID)}
+//   type="button"
+// ></button>
+
+// You should end up with an inlined JavaScript ES6 arrow function with access to the objectID property of the item object.
+
+// Using arrow functions in event handlers directly impacts your application’s performance. For instance, the onClick handler for the onDismiss() method wraps the method in another arrow function to pass the item identifier. Every time the render() method runs, the handler instantiates the higher-order arrow function. It can have an impact on your application performance, but in most cases you won’t notice. If you have a huge table of data with 1000 items and each row or column has an arrow function in an event handler, it is worth thinking about the performance implications.
+
+// so you could implement a dedicated Button component to bind the method in the constructor (of the button component only once). Before that, though, it is premature optimization, and it is more prudent to learn the basics of React before thinking about optimization.
